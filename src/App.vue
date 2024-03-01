0b79a2f88831b10983988135ca38cff58b91c420
DHTMLX <155955527+dhtmlx1@users.noreply.github.com>
2024-03-01 03:19:06 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import Spreadsheet from "./components/Spreadsheet.vue";
+import { getData } from "./data";
+
+export default {
+  components: { Spreadsheet },
+  data() {
+    return { data: getData() };
+  },
+};
+</script>
+
+<template>
+  <Spreadsheet :data="data" />
+</template>
---
+++ src/components/Spreadsheet.vue
@@ -0,0 +1,26 @@
+<script>
+import { Spreadsheet } from "@dhx/trial-spreadsheet";
+import "@dhx/trial-spreadsheet/codebase/spreadsheet.min.css";
+
+export default {
+  props: ["data"],
+  data() {
+    return {
+      spreadsheet: null,
+    };
+  },
+
+  mounted() {
+    this.spreadsheet = new Spreadsheet(this.$refs.cont, {});
+    this.spreadsheet.parse(this.data);
+  },
+  unmounted() {
+    this.spreadsheet.destructor();
+    this.$refs.cont.innerHTML = "";
+  },
+};
+</script>
+
+<template>
+  <div ref="cont" style="width: 100%; height: 100%"></div>
+</template>
